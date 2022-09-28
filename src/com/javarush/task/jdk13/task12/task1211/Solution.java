7169978af8f54a7992467713400710b9ae0228ef
Serge Kravchenko <0634380033@ukr.net>
2022-09-28 19:41:44 +0300

    task1211
    task1211

---
+++ src/com/javarush/task/jdk13/task12/task1211/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.jdk13.task12.task1211;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(new Integer((1)));
+        print(2);
+
+    }
+
+    public static void print(int a) {
+
+    }
+
+    public static void print(Integer s) {
+
+    }
+}
